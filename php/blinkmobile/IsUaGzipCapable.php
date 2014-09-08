bb32258e3153c5c6ea0c8c00b18911d0472a8b9b
Ron Waldon <jokeyrhyme@gmail.com>
2014-09-08 17:12:04 +1000

    implement Apache HTTPD notes and IE11 bug

--- php/blinkmobile/IsUaGzipCapable.php
+++ php/blinkmobile/IsUaGzipCapable.php
@@ -4,5 +4,49 @@ namespace blinkmobile;
 
 class IsUaGzipCapable
 {
+    private static function testAcceptEncoding($acceptEncoding = null)
+    {
+        return is_string($acceptEncoding) && stripos($acceptEncoding, 'gzip') !== false;
+    }
 
+    private static function testUserAgentString($userAgent = null)
+    {
+        if (empty($userAgent) || !is_string($userAgent)) {
+            return true;
+        }
+        if (stripos($userAgent, 'MSIE') !== false ||  stripos($userAgent, 'Trident') !== false) {
+            if (stripos($userAgent, 'rv:11.0') !== false) {
+                // https://connect.microsoft.com/IE/feedbackdetail/view/950689
+                return false;
+            }
+            // http://httpd.apache.org/docs/2.0/en/mod/mod_deflate.html
+            return true;
+        }
+
+        if (stripos($userAgent, 'Mozilla/4') === 0) {
+            // http://httpd.apache.org/docs/2.0/en/mod/mod_deflate.html
+            // technically we should still GZIP text/html, but this is easier
+            return false;
+        }
+        return true;
+    }
+
+    public static function testRequest($_server = null)
+    {
+        if ($_server == null || !is_array($_server)) {
+            if (!empty($_SERVER)) {
+                $_server = $_SERVER;
+            } else {
+                $_server = array(
+                    'HTTP_ACCEPT_ENCODING' => '',
+                    'HTTP_USER_AGENT' => ''
+                );
+            }
+        }
+        if (!self::testAcceptEncoding($_server['HTTP_ACCEPT_ENCODING'])) {
+            // never GZIP if the user agent doesn't accept it
+            return false;
+        }
+        return self::testUserAgentString($_server['HTTP_USER_AGENT']);
+    }
 }
--- tests/php/blinkmobile/IsUaGzipCapableTest.php
+++ tests/php/blinkmobile/IsUaGzipCapableTest.php
@@ -9,4 +9,66 @@ class IsUaGzipCapableTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertTrue(class_exists('\blinkmobile\IsUaGzipCapable'));
     }
+
+    public function testTestMethodExists()
+    {
+        $this->assertTrue(method_exists('\blinkmobile\IsUaGzipCapable', 'testRequest'));
+    }
+
+    public function testEmptyAcceptEncodingDoesNotGzip()
+    {
+        $this->assertFalse(IsUaGzipCapable::testRequest(array(
+            'HTTP_ACCEPT_ENCODING' => '',
+            'HTTP_USER_AGENT' => ''
+        )));
+    }
+
+    /**
+     * http://httpd.apache.org/docs/2.0/en/mod/mod_deflate.html
+     */
+    public function testNetscape4OnlyGzipsTextHtml()
+    {
+        // TODO: make the tests sensitive to response MIME type somehow
+    }
+
+    /**
+     * http://httpd.apache.org/docs/2.0/en/mod/mod_deflate.html
+     */
+    public function testNetscape406To408DoesNotGzip()
+    {
+        $this->assertFalse(IsUaGzipCapable::testRequest(array(
+            'HTTP_ACCEPT_ENCODING' => 'gzip',
+            'HTTP_USER_AGENT' => 'Mozilla/4.06 [de] (WinNT; I)'
+        )));
+        $this->assertFalse(IsUaGzipCapable::testRequest(array(
+            'HTTP_ACCEPT_ENCODING' => 'gzip',
+            'HTTP_USER_AGENT' => 'Mozilla/4.07 [de] (WinNT; I)'
+        )));
+        $this->assertFalse(IsUaGzipCapable::testRequest(array(
+            'HTTP_ACCEPT_ENCODING' => 'gzip',
+            'HTTP_USER_AGENT' => 'Mozilla/4.08 [de] (WinNT; I)'
+        )));
+    }
+
+    /**
+     * http://httpd.apache.org/docs/2.0/en/mod/mod_deflate.html
+     */
+    public function testInternetExplorerDoesGzip()
+    {
+        $this->assertTrue(IsUaGzipCapable::testRequest(array(
+            'HTTP_ACCEPT_ENCODING' => 'gzip',
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows; U; MSIE 9.0; WIndows NT 9.0; en-US))'
+        )));
+    }
+
+    /**
+     * https://connect.microsoft.com/IE/feedbackdetail/view/950689
+     */
+    public function testInternetExplorer11DoesNotGzip()
+    {
+        $this->assertFalse(IsUaGzipCapable::testRequest(array(
+            'HTTP_ACCEPT_ENCODING' => 'gzip',
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'
+        )));
+    }
 }
